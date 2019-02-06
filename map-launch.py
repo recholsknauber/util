0852fe8b0a11602e1dd8048ee5cd00a14ab078b6
Ryan Knauber <ryanknauber@yahoo.com>
2019-02-06 11:13:48 -0800

    "Fixed."

--- map-launch.py
+++ map-launch.py
@@ -1,9 +1,9 @@
 #map_launch.py - Launches a map in the browser using an address from the command line or clipboard
 
-import webbrowser, sys, paperclip
+import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
     # Get address from command line
-    address = ' '.join(sysargv[1:])
+    address = ' '.join(sys.argv[1:])
 else:
     # Get address from clipboard.
     address = pyperclip.paste()
