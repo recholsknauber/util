82515eb70231ea50e19d9e5c200eb625ecaea477
Ryan Knauber <ryanknauber@yahoo.com>
2019-12-02 10:22:56 -0800

    <(-.-<)

--- 1build/random.el
+++ 1build/random.el
@@ -6,6 +6,7 @@
 (cdr '(olivia liam ryan))
 (cons 'pine '(fir oak maple))
 
+(nthcdr 2 '(pine fir oak maple))
 (nthcdr 3 '(pine fir oak maple))
 (nthcdr 4 '(pine fir oak maple))
 
@@ -32,4 +33,17 @@ allbirdteams
 (setcar allbirdteams 'losers)
 allbirdteams
 
-(zap-to-char
+(cons "we reflect on"
+      '("the effort that brought us" "this food"))
+
+     (defun test-zap-to-char (arg char)
+       "Kill up to and including ARG'th occurrence of CHAR.
+     Case is ignored if `case-fold-search' is non-nil in the current buffer.
+     Goes backward if ARG is negative; error if CHAR not found."
+       (interactive "p\ncZap to char: ")
+       (if (char-table-p translation-table-for-input)
+           (setq char (or (aref translation-table-for-input char) char)))
+       (kill-region (point) (progn
+                              (search-forward (char-to-string char)
+                                              nil nil arg)
+                              (point))))
