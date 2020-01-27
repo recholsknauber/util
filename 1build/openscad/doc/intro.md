8f16bb135647507e1059265fa8381bc0b908dc4c
Ryan Knauber <ryanknauber@yahoo.com>
2020-01-27 10:34:33 -0800

    <(-.-<)

---
+++ 1build/openscad/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to openscad
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ 1build/openscad/test/openscad/core_test.clj
@@ -0,0 +1,7 @@
+(ns openscad.core-test
+  (:require [clojure.test :refer :all]
+            [openscad.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
